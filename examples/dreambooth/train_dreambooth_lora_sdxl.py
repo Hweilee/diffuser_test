bbb29a0bd61cafc68f1e538ccdf6e80ab4a7a5d7
蒋硕 <jiangshuo9@h-partners.com>
2024-11-14 08:51:03 +0800

    Fix issues for saving checkpointing steps

--- examples/dreambooth/train_dreambooth_lora_sdxl.py
+++ examples/dreambooth/train_dreambooth_lora_sdxl.py
@@ -1844,7 +1844,7 @@ def main(args):
                                 for removing_checkpoint in removing_checkpoints:
                                     removing_checkpoint = os.path.join(args.output_dir, removing_checkpoint)
                                     shutil.rmtree(removing_checkpoint)
-    
+
                         save_path = os.path.join(args.output_dir, f"checkpoint-{global_step}")
                         accelerator.save_state(save_path)
                         logger.info(f"Saved state to {save_path}")
